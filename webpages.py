dc628211e627325ea22d57cbf39167f3eb5a1cb6
wesleychiau <wesleychiau@gmail.com>
2023-03-13 00:23:11 +0800

    Converted code logic, added search bar template

--- webpages.py
+++ webpages.py
@@ -1,106 +1,113 @@
 import os
 import shutil
+from bs4 import BeautifulSoup
 
-# Generate the output file based on the template chosen
-def generateOutputFile(template, output):
-    # Get the current directory
-    currentDir = os.getcwd()
 
+def generateOutputFile(template, output, sourceFile=None):
     try:
-        # Get the source template
-        sourceFile = os.path.join(currentDir, "templates" , template)
+        if sourceFile:
+            shutil.copy2(sourceFile, output)
+        else:
+            shutil.copy2(template, output)
 
-        # Combine the current directory and the new file name to get the full path of the output file
-        outputFile = os.path.join(currentDir, output)
-
-        # Copy the file to the current directory and rename it
-        shutil.copy2(sourceFile, outputFile)
     except IOError:
         print("(generateOutputFile) Unable to open file.")
+        return None
 
-    return outputFile
+    return output
 
-# Insert element into output file based on source file path
-def insertElement(sourceFilePath, outputFilePath, elementToInsert, beforeElement):
+
+def extractElement(htmlFilePath, element):
     try:
-        # Open source file to extract element and all its containing data
-        with open(sourceFilePath, 'r', encoding='utf-8') as sourceFile:
-            sourceHtml = sourceFile.read()
+        with open(htmlFilePath, 'r', encoding='utf-8') as htmlFile:
+            soup = BeautifulSoup(htmlFile.read(), 'html.parser')
+            mainElement = soup.find(element)
+            if mainElement:
+                return str(mainElement)
 
-            # Get the start and end index of the element
-            startIndex = sourceHtml.index(f'<{elementToInsert}>')
-            endIndex = sourceHtml.index(f'</{elementToInsert}>', startIndex) + len(f'</{elementToInsert}>')
+    except IOError:
+        print(f"Unable to open file {htmlFilePath}.")
+    
+    return None
 
-            # Extract the element from the source HTML
-            elementHtml = sourceHtml[startIndex:endIndex]+"\n"
-            sourceFile.close()
+
+def insertOrReplaceElement(outputFilePath, elementToInsertTag, afterElementTag, elementData):
+    try:
+        # Decode the element data
+        elementData = BeautifulSoup(elementData, 'html.parser')
 
         # Open output file to insert element
         with open(outputFilePath, 'r+', encoding='utf-8') as outputFile:
             # Read the contents of the output file
             outputHtml = outputFile.read()
 
-            # Find the index of the element that is after what is meant to be inserted
-            startIndex = outputHtml.index(f'<{beforeElement}>')
+            # Create a BeautifulSoup object
+            soup = BeautifulSoup(outputHtml, 'html.parser')
 
-            # Insert the data before the element
-            outputHtml = outputHtml[:startIndex] + elementHtml + outputHtml[startIndex:]
+            # Find the element to insert after
+            afterElement = soup.find(afterElementTag)
 
+            # Find the element that to be inserted
+            insertElement = soup.find(elementToInsertTag)
+
+            # If the element to insert is not found, insert the new element after the "afterElement"
+            if (not insertElement) and afterElement:
+                afterElement.insert_after(elementData)
+                print("Element inserted after header.")
+            # If element to insert is not found and no "afterelement", insert into the body.
+            elif not insertElement:
+                soup.find('body').insert_after(elementData)
+                print("Element inserted after body.")
+            # If the element is found, replace the new element at the end of the body
+            else:
+                insertElement.replace_with(elementData)
+                print("Element replaced.")
+                
             # Write the modified HTML to the output file
-            outputFile.seek(0) 
-            outputFile.write(outputHtml)
-            outputFile.truncate()  # Remove any remaining content after the new data
+            outputFile.seek(0)
+            outputFile.write(str(soup))
+            outputFile.truncate()
 
-            print(f"Element '{elementToInsert}' added to '{outputFilePath}'")
-            outputFile.close()
-
+            print(f"Element '{elementToInsertTag}' added to '{outputFilePath}'")
+            
     except IOError:
         print("(insertElement) Unable to open file.")
 
-# To insert data into an HTML element in a file.
-def insertDataIntoElement(element, dataInsert, filePath):
-    # Flag to track if the element is found in the input file
-    lineFound = False
-    try:
-        # Open the input file and read all lines into a list
-        with open(filePath, 'r+', encoding='utf-8') as file:
-            lines = file.readlines()
 
-            # Look for the specified element in the lines and add the data to it
-            for i in range(len(lines)):
-                if element.lower() in lines[i].lower():
-                    print(f"Adding line {dataInsert} to file after element {element}")
-                    lines[i] = lines[i].replace(f"<{element}>", f"<{element}>{dataInsert}")
-                    lineFound = True
-                    break
-            
-            # If the specified element was found in the input file, write the modified lines to the output file
-            if lineFound:
-                file.seek(0)
-                file.writelines(lines)
-            # If the specified element was not found in the input file, print an error message
+def generateHoneypotPage(template, sourceFilePath, outputFile):
+    try:
+        # Get the current directory
+        currentDir = os.getcwd()
+
+        # Get the source template
+        templateFile = os.path.join(currentDir, "templates", template)
+
+        # Combine the current directory and the new file name to get the full path of the output file
+        outputFile = os.path.join(currentDir, outputFile)
+
+        # Generate output file first based on template file
+        outputfile = generateOutputFile(templateFile, outputFile, sourceFilePath)
+
+        if outputfile:
+            # Extract the main element from the template file
+            templatemain = extractElement(templateFile, 'main')
+
+            if templatemain:
+                # Add main to output file, placed before footer if main is not present.
+                insertOrReplaceElement(outputfile, "main", "header", templatemain)
+
             else:
-                print(f"Element '{element}' not found in file {filePath}")
-            file.close()
+                print(f"Unable to extract main element from {templateFile}.")
                 
-    # If there was an error opening the input file, print an error message
+        else:
+            print(f"Unable to generate output file {outputFile}.")
+    
     except IOError:
-        print("(insertDataIntoElement) Unable to open file.")
+        print("(generateAdminLoginPage) Unable to open file.")
 
 if __name__ == '__main__':
-    template = "AdminLoginPageTemplate.html"
+    template = "SecretSearchPage.html"
     sourceFilePath = "source.html"
-    # Generate output file first based on template file
-    outputfile = generateOutputFile(template, "output.html")
+    outputFile = "output.html"
 
-    # Add head to output file, placed before <body>
-    insertElement(sourceFilePath,outputfile,"head","body")
-
-    # Add header to output file, placed before <main>
-    insertElement(sourceFilePath,outputfile,"header","main")
-
-
-    # Add footer to output file, placed before </body>
-    insertElement(sourceFilePath,outputfile,"footer","/body")
-
-    print (f"Output file generated. File name: {outputfile}")
+    generateHoneypotPage(template, sourceFilePath, outputFile)
