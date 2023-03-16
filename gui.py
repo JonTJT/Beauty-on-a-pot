5b44f9e9722c5bc910cfcc6c478b7ee30032e22d
Gian Meng Rong <23660665+GMengRong@users.noreply.github.com>
2023-03-16 15:12:05 +0800

    implemented GUI to work with templates methods in func.py

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import func as fn
+import os
 import tkinter as tk
 from tkinter import font  as tkfont
 from tkinter import Message, Widget, filedialog , messagebox , ttk
@@ -6,14 +7,35 @@ from tkinter.constants import BOTH, BOTTOM, CENTER, DISABLED, FALSE, LEFT, NORMA
 
 def generate():
     fn.server = clicked.get()
-    fn.folderpath = folderfld.get()
     fn.logfile = logfld.get()
-    fn.genFiles()
+
+    source = srcfld.get()
+    root_ext = ""
+    if source == "":
+        root_ext = os.path.splitext("default.html")
+    else:
+        root_ext = os.path.splitext(source)
+
+    template = ""
+    outputFile = ""
+    if templateClicked.get() == "Login":
+        template = "AdminLoginPageTemplate.html"
+        outputFile = root_ext[0] + "_AdminLoginHoneypot" + root_ext[1]
+    elif templateClicked.get() == "Search":
+        template = "SecretSearchPage.html"
+        outputFile = root_ext[0] + "_SearchHoneypot" + root_ext[1]
+    
+    fn.generateHoneypotPage(template, source, outputFile)
 
 def getDir(fld):
     location = filedialog.askdirectory()
     fld.delete(0,END)
     fld.insert(0,location)
+
+def getSrc(fld):
+    srcFile = filedialog.askopenfilename()
+    fld.delete(0,END)
+    fld.insert(0,srcFile)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -23,40 +45,49 @@ if __name__ == "__main__":
     MainTitle.pack()
     serverFrame = tk.Frame(root)
     serverFrame.pack()
-    folderFrame = tk.Frame(root)
-    folderFrame.pack()
     logFrame = tk.Frame(root)
     logFrame.pack()
+    templateFrame = tk.Frame(root)
+    templateFrame.pack()
+    srcFrame = tk.Frame(root)
+    srcFrame.pack()
 
     # Labels
     serverlbl = ttk.Label(serverFrame,text="Server:",font=('Courier',13,'bold'))
     serverlbl.grid(row = 1, column = 0, sticky='e')
-    folderlbl = ttk.Label(folderFrame,text="Honeypot Webpage Folderpath:",font=('Courier',13,'bold'))
-    folderlbl.grid(row = 1, column = 0, sticky='e')
     loglbl = ttk.Label(logFrame,text="Logfile Folderpath:",font=('Courier',13,'bold'))
     loglbl.grid(row = 1, column = 0, sticky='e')
+    templatelbl = ttk.Label(templateFrame,text="Honeypot Template:",font=('Courier',13,'bold'))
+    templatelbl.grid(row = 1, column = 0, sticky='e')
+    srclbl = ttk.Label(srcFrame,text="Source File:",font=('Courier',13,'bold'))
+    srclbl.grid(row = 1, column = 0, sticky='e')
 
     # server = OptionMenu()
-    # folderpath = Entry()
     # logfile = Entry()
     options = ["Apache", "Nginx"]
     clicked = tk.StringVar()
     clicked.set("Apache")
-
     serverfld = tk.OptionMenu(serverFrame, clicked, *options )
     serverfld.grid(row = 1, column = 1, sticky='e')
-    folderfld = tk.Entry(folderFrame, text="")
-    folderfld.grid(row = 1, column = 1, sticky='e')
     logfld = tk.Entry(logFrame, text="")
     logfld.grid(row = 1, column = 1, sticky='e')
 
+    templateOptions = ["Login", "Search"]
+    templateClicked = tk.StringVar()
+    templateClicked.set("Login")
+    templatefld = tk.OptionMenu(templateFrame, templateClicked, *templateOptions )
+    templatefld.grid(row = 1, column = 1, sticky='e')
+    srcfld = tk.Entry(srcFrame, text="")
+    srcfld.grid(row = 1, column = 1, sticky='e')
+
     # Btn for directory locations
-    folderBtn = tk.Button(folderFrame, text="Select directory", command=lambda:getDir(folderfld))
-    folderBtn.grid(row = 1, column = 2, sticky='e')
     logBtn = tk.Button(logFrame, text="Select directory", command=lambda:getDir(logfld))
     logBtn.grid(row = 1, column = 2, sticky='e')
+    
+    srcBtn = tk.Button(srcFrame, text="Select Source File", command=lambda:getSrc(srcfld))
+    srcBtn.grid(row = 1, column = 2, sticky='e')
 
-    logBtn = tk.Button(root, text="Generate", command=lambda:generate())
-    logBtn.pack()
-
+    genBtn = tk.Button(root, text="Generate", command=lambda:generate())
+    genBtn.pack()
+    
     root.mainloop()
