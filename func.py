e68806a3cef218e72aaf2ce32096de558f2061dc
Vynorimas <jontanjunting@gmail.com>
2023-03-19 02:19:04 +0800

    bugfix for has_existing_apache_configuration() function

--- func.py
+++ func.py
@@ -294,16 +294,20 @@ def has_existing_apache_configuration(file_path, unique_strings, sec_audit_log_p
         print(f"The file '{file_path}' does not exist. Skipping the configuration check.")
         return False
 
+    sec_audit_log_line_found = False
     for unique_string in unique_strings:
         if unique_string != "SecAuditLog":
             if any(unique_string in line for line in lines):
                 return True
         else:
-            sec_audit_log_line = f"SecAuditLog {sec_audit_log_path}"
-            if sec_audit_log_line in lines:
-                return True
+            for line in lines:
+                if "SecAuditLog" in line:
+                    sec_audit_log_line_found = True
+                    if sec_audit_log_path in line:
+                        return True
 
-    return False
+    return sec_audit_log_line_found and any("SecAuditLog" in line for line in lines)
+
 
 
 def create_honeypot_rules(sec_audit_log_path):
